a95327e0e89ca013aefce58c3e4d047a5ab55e72
Robert Zurga <bitbucket@zurga.com>
2016-04-22 01:31:15 -0500

    Added virtualenv setup and link  of it into dev to be backed up to a shared folder

--- recipes/activate_virtualenvwrapper.rb
+++ recipes/activate_virtualenvwrapper.rb
@@ -11,6 +11,10 @@ include_recipe "chef-solo-search"
 
 node[:users].each do |user_id|
     user_entry = search(:users, "id:" + user_id).first
+    group_name = user_entry["group"] || node[:machine][:group] || user_id
+    user_uid = user_entry["uid"] || node[:machine][:uid]
+    home_dir = user_entry["home"] || node[:machine][:home] || "/home/" + user_id
+    dev_dir = user_entry["dev"] || node[:machine][:dev] || home_dir + "/dev"
     virtualenvwrapper =  = user_entry["virtualenvwrapper"]
     
     unless virtualenvwrapper.nil?
@@ -19,5 +23,10 @@ node[:users].each do |user_id|
             code "source virtualenvwrapper.sh"
             action :run
         end
+    end
+    
+    link dev_dir + 'virtualenv' do
+        owner user_id
+        to home_dir + '/.virtualenv'
     end
 end
