a2b80842620c1d2a3eb2b9d8e9fae9b66d9b93dc
Robert Zurga <bitbucket@zurga.com>
2016-04-23 23:06:44 -0500

    Fix virtualenvrapper install

--- recipes/activate_virtualenvwrapper.rb
+++ recipes/activate_virtualenvwrapper.rb
@@ -7,8 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# include_recipe "chef-solo-search"
-
 node[:users].each do |user_id|
     user_entry = search(:users, "id:" + user_id).first
     group_name = user_entry["group"] || node[:machine][:group] || user_id
@@ -17,17 +15,23 @@ node[:users].each do |user_id|
     dev_dir = user_entry["dev"] || node[:machine][:dev] || home_dir + "/dev"
     virtualenvwrapper = user_entry["virtualenvwrapper"]
     
-    # unless virtualenvwrapper.nil?
-    #     bash "activate virtualenvwrapper" do
-    #         user user_id
-    #         code "source virtualenvwrapper.sh"
-    #         action :run
-    #     end
-    # end
+    unless virtualenvwrapper.nil?
+        bash "activate virtualenvwrapper" do
+            user user_id
+            cwd home_dir
+            code <<-EOD
+                VIRTUALENVWRAPPER=$(which virtualenvwrapper.sh)
+                # Fix reference ${.sh.file} in line #95
+                sudo sed -i "s/{.sh.file}/{BASH}/g" $VIRTUALENVWRAPPER
+                sudo su - #{user_id} -c $VIRTUALENVWRAPPER
+            EOD
+            action :run
+        end
+    end
     
-    # link dev_dir + '/virtualenv' do
-    #     group group_name
-    #     owner user_id
-    #     to home_dir + '/.virtualenv'
-    # end
+    link dev_dir + '/virtualenv' do
+        group group_name
+        owner user_id
+        to home_dir + '/.virtualenv'
+    end
 end
